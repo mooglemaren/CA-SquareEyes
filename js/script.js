40c8664223cb808b6a79e64a1bd995cb18f54bb9
mooglemaren <144604598+mooglemaren@users.noreply.github.com>
2024-02-18 22:07:19 +0100

    hentet og slicet utvalg til forside og stylet litt på utvalget

--- js/script.js
+++ js/script.js
@@ -1,46 +1,47 @@
-console.clear();
+const products=[]
 
-async function getData() {
-  try{
+async function getAllProducts() {
+  try {
     const api = `https://v2.api.noroff.dev/square-eyes`;
     const response = await fetch(api);
-    //console.log(api)
+    console.log(api);
     if (!response.ok) throw new Error(`HTTP error! ${response.status}`);
     const obj = await response.json();
-    //console.log(obj);    
+    console.log(obj);
+    return obj.data;
   } catch (error) {
-    //console.error(error.message); 
+    console.error(error.message);
+    alert("Could not fetch data");
   }
 }
 
-getData();
+async function initMainPage() {
+  const productsResponse = await getAllProducts();
+  if (!productsResponse) return;
+  productsResponse.forEach(movie =>products.push(movie));
+  const frontPageProducts = products.slice(0, 3);
+  console.log(frontPageProducts);
+  frontPageProducts.forEach(movie =>{
+    addMovieToFrontpage(movie)
+  })
+}
 
-const api = `https://v2.api.noroff.dev/square-eyes`;
-fetch(api)
-    .then ((response) => {
-        if (!response.ok) throw new Error(`HTTP error! ${response.status}`);
-        return response.json();
-    })
-    .then ((obj) => console.log(obj))
-    .catch (error => console.error(error.message))
+function addMovieToFrontpage(movie) {
+  const movieSection = document.getElementById("frontpage_movies");
+  const link = document.createElement("a");
+  const figure = document.createElement("figure");
+  const poster = document.createElement("img");
+  const figcaption = document.createElement("figcaption");
+  poster.src = movie.image?.url;
+  poster.alt = movie.image?.alt;
+  figcaption.innerHTML = "View now!";
+  link.href = "/product.html";
+  figure.appendChild(poster);
+  figure.appendChild(figcaption);
+  link.appendChild(figure);
+  movieSection.appendChild(link);
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-/*async function getData() {
-    const api = "https://api.noroff.dev/api/v2/square-eyes";
-    console.log(api);
-
-    const response = await fetch(api);
-    const obj = await response.json();
-    }
-    getData();*/
+window.addEventListener("DOMContentLoaded", function () {
+  initMainPage();
+});
